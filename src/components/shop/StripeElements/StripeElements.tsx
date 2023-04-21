73a402f2a20a50b4ab5c22436c2e994fa3dc18f9
Yanto Lee <yanto3528@gmail.com>
2023-04-21 08:39:00 +0700

    fix: stripe build error

--- src/components/shop/StripeElements/StripeElements.tsx
+++ src/components/shop/StripeElements/StripeElements.tsx
@@ -5,7 +5,7 @@ import { loadStripe } from '@stripe/stripe-js';
 
 import { StripeElementsProps } from './StripeElements.types';
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
 
 export default function StripeElements({ children, clientSecret }: StripeElementsProps) {
   return (
